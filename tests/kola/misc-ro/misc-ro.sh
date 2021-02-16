e71c965f7e94b15eae38a8af3467dd521bb5e46d
Renata Ravanelli <rravanel@redhat.com>
2021-02-16 10:39:54 -0300

    Remove repo check for NM using RHEL8.4
    Signed-off-by: Renata Ravanelli <rravanel@redhat.com>

--- tests/kola/misc-ro/misc-ro.sh
+++ tests/kola/misc-ro/misc-ro.sh
@@ -133,9 +133,6 @@ echo "ok iSCSI initiator name"
 
 # Let's make sure the NetworkManager we use is one of the one-off
 # rebuilds while we're following RHEL 8.3.
-if [[ ! $(rpm -q NetworkManager) =~ 'rhaos4.7' ]]; then
-    fatal "NetworkManager package changed from rhaos4.7 branch. Needs investigation."
-fi
 echo "ok NetworkManager package comes from rhaos4.7 branch."
 
 # Ensure NM's internal DHCP client runs by default
